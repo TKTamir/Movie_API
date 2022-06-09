88fb26dd3e64d01270c1f60f842bb3e71a671ef6
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-09 12:07:59 +0200

    Create get function for top Movies

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
+
 const app = express();
+
+let topMovies = [
+  {
+    title: 'The Shawshank Redemption',
+    year: '1994',
+  },
+  {
+    title: 'The Godfather',
+    year: '1972',
+  },
+  {
+    title: 'The Dark Knight',
+    year: '2008',
+  },
+  {
+    title: "The Godfather Part II",
+    year: '1974',
+  },
+  {
+    title: '12 Angry Men',
+    year: '1957',
+  },
+  {
+    title: 'Schindlers List',
+    year: '1993',
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: '2003',
+  },
+  {
+    title: 'Pulp Fiction',
+    year: '1994',
+  },
+  {
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    year: '2001',
+  },
+  {
+    title: 'The Good, the Bad and the Ugly',
+    year: '1966',
+  },
+];
+
+//GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movies API');
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies)
+});
+
+//Listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
