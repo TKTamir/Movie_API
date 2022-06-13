325e5cba47d8bdc9e844996db02f9b3488ffcff7
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-13 16:24:43 +0200

    Add genre, director, imgurl to movies

--- index.js
+++ index.js
@@ -16,42 +16,72 @@ let movies = [
   {
     title: 'The Shawshank Redemption',
     year: '1994',
+    genre:'Drama',
+    director:'Frank Darabont',
+    imageurl:'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i',
   },
   {
     title: 'The Godfather',
     year: '1972',
+    genre:'Crime',
+    director:'Francis Ford Coppola',
+    imageurl:'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
   },
   {
     title: 'The Dark Knight',
     year: '2008',
+    genre:'Action',
+    director:'Christopher Nolan',
+    imageurl:'imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i',
   },
   {
     title: 'The Godfather Part II',
     year: '1974',
+    genre:'Crime',
+    director:'Francis Ford Coppola',
+    imageurl:'https://www.imdb.com/title/tt0071562/mediaviewer/rm4159262464/?ref_=tt_ov_i',
   },
   {
     title: '12 Angry Men',
     year: '1957',
+    genre:'Crime',
+    director:'Sidney Lumet',
+    imageurl:'https://www.imdb.com/title/tt0050083/mediaviewer/rm2927108352/?ref_=tt_ov_i',
   },
   {
     title: 'Schindlers List',
     year: '1993',
+    genre:'Biography',
+    director:'Steven Spielber',
+    imageurl:'https://www.imdb.com/title/tt0108052/mediaviewer/rm1610023168/?ref_=tt_ov_i',
   },
   {
     title: 'The Lord of the Rings: The Return of the King',
     year: '2003',
+    genre:'',
+    director:'Peter Jackson',
+    imageurl:'https://www.imdb.com/title/tt0167260/mediaviewer/rm584928512/?ref_=tt_ov_i',
   },
   {
     title: 'Pulp Fiction',
     year: '1994',
+    genre:'Crime',
+    director:'Quentin Tarantino',
+    imageurl:'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/?ref_=tt_ov_i',
   },
   {
     title: 'The Lord of the Rings: The Fellowship of the Ring',
     year: '2001',
+    genre:'Action',
+    director:'Peter Jackson',
+    imageurl:'https://www.imdb.com/title/tt0120737/mediaviewer/rm3592958976/?ref_=tt_ov_i',
   },
   {
     title: 'The Good, the Bad and the Ugly',
     year: '1966',
+    genre:'Adventure',
+    director:'Sergio Leone',
+    imageurl:'https://www.imdb.com/title/tt0060196/mediaviewer/rm1383786241/?ref_=tt_ov_i',
   },
 ];
 
@@ -73,6 +103,43 @@ app.get('/movies/:title', (req , res) => {
     res.status(400).send('Movie not found')
   }
 })
+
+//READ Title
+app.get('/movies/:title', (req , res) => {
+  const { title } = req.params;
+  const movie = movies.find( movie => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('Movie not found')
+  }
+})
+
+//READ Genre
+app.get('/movies/genre/:genreName', (req , res) => {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.Genre.Name === genreName ).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('Genre not found')
+  }
+})
+
+//READ Directors
+app.get('/movies/directors/:directorName', (req , res) => {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.Director.Name === directorName ).Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send('Director not found')
+  }
+})
+
 
 app.listen(8080, () => console.log("Listening on 8080"))
 
