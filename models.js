09b9c4344df75ddfe28286f89f8cc26df744a996
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-22 18:06:57 +0200

    Hash password in user Schema

--- models.js
+++ models.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
+//Define Movies Schema
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
@@ -19,7 +21,7 @@ let movieSchema = mongoose.Schema({
   ImagePath: String,
   Featured: Boolean,
 });
-
+//Define UserSchema
 let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
@@ -27,6 +29,15 @@ let userSchema = mongoose.Schema({
   Birthdate: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
+//Function to hash password
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+//Function to validate password
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
