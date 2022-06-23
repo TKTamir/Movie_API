710cd0cb64c6236da1fb930aa3276905b002790e
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-23 10:22:38 +0200

    Update Port

--- index.js
+++ index.js
@@ -232,4 +232,8 @@ app.use((err, req, res, next) => {
   res.status(500).send('Error');
 });
 
-app.listen(8080, () => console.log('Listening on 8080'));
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
+});
