64e9e28011b298bcdb1f34665468ad4319c83f3e
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-27 17:31:40 +0200

    Update express.static function

--- index.js
+++ index.js
@@ -263,8 +263,8 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 });
 
 //Access documentation.html through express.static
-app.get('/documentation', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/documentation.html' + '/public/styles.css'));
+app.get('/public', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/documentation.html'));
 });
 
 //Error handling
