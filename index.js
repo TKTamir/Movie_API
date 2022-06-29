516f0888997d45d20da9b9c658aa2f190fd12193
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-29 11:18:07 +0200

    Update cors policy

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(
       if (allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin isn’t found on the list of allowed origins
         let message =
-          'The CORS policy for this application doesn’t allow access from origin ' + origin;
+          'The CORS policy for this application does not allow access from origin ' + origin;
         return callback(new Error(message), false);
       }
       return callback(null, true);
