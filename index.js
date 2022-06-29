eb812ae536efe30f63e6ea357753b36a19c450a8
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-06-29 11:24:40 +0200

    Adjust get function without jwt token

--- index.js
+++ index.js
@@ -53,17 +53,29 @@ app.get('/', (req, res) => {
 });
 
 //READ-get- Get all movies
-app.get('/movies' /*passport.authenticate('jwt', { session: false }*/),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(200).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  } /*)*/;
+// app.get('/movies', passport.authenticate('jwt', { session: false }),
+//   (req, res) => {
+//     Movies.find()
+//       .then((movies) => {
+//         res.status(200).json(movies);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send('Error: ' + err);
+//       });
+//   });
+
+//TEMPORARY READ-get- Get all movies
+app.get('/movies', function (req, res) {
+  Movies.find()
+    .then(function (movies) {
+      res.status(200).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
 
 //READ Title-get- Get a movie by title
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
