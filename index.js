5bf401377565762a4c16e18ef44c2e1a973d3ae5
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-07-13 18:47:34 +0200

    Update allowed origins to new url

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 let allowedOrigins = [
   'http://localhost:8080',
   'http://localhost:1234',
-  'https://main--spotflix.netlify.app/',
+  'https://main--appformovies.netlify.app/',
 ];
 const passport = require('passport');
 require('./passport');
